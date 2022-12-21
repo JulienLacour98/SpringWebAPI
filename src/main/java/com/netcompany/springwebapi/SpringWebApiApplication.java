59bf7094a6f44c68ff68ff8d8d5261fe9c559f86
Julien Lacour <jlac@netcompany.com>
2022-12-21 10:49:50 +0100

    Initial commit

---
+++ src/main/java/com/netcompany/springwebapi/SpringWebApiApplication.java
@@ -0,0 +1,13 @@
+package com.netcompany.springwebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/netcompany/springwebapi/SpringWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netcompany.springwebapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
